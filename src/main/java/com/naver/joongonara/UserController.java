8c50ab8bbaf163a3a9d896a66759f77722d0f7ac
Han Manjong <aksmj8855@gmail.com>
2018-08-02 12:26:22 +0900

    Add validation check for signUp

--- src/main/java/com/naver/joongonara/UserController.java
+++ src/main/java/com/naver/joongonara/UserController.java
@@ -1,7 +1,11 @@
 package com.naver.joongonara;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -12,7 +16,32 @@ public class UserController {
 
     @PostMapping
     @ResponseBody
-    User signUp(@RequestBody User newUser) {
-        return userService.signUp(newUser);
+    ResponseEntity<User> signUp(@RequestBody User newUser) {
+
+        if (!isValidAge(newUser.getAge())) {
+            // Bad Request 400
+            return ResponseEntity.badRequest().build();
+        }
+
+        if (!isValidEmailAddress(newUser.getEmail())) {
+            // Bad Request 400
+            return ResponseEntity.badRequest().build();
+        }
+        // Create User
+        User createdUser = userService.signUp(newUser);
+
+        // Return response
+        return ResponseEntity.ok(createdUser);
+    }
+
+    Boolean isValidAge(int age) {
+        return (age >= 12) && (age <= 70);
+    }
+
+    Boolean isValidEmailAddress(String emailAddress) {
+        Pattern emailAddressRegex = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = emailAddressRegex .matcher(emailAddress);
+
+        return matcher.find();
     }
 }
